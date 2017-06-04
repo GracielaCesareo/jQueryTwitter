83aa66d1efd4b1e803079504d31803351f3a9947
Graciela Cesareo Cruz <gracielacesareocruz@Gracielas-MacBook-Pro.local>
2017-06-04 14:22:07 -0500

    primera version twitter

---
+++ js/app.js
@@ -0,0 +1,42 @@
+(function () {
+	var contador = 0;
+
+	var cargarPagina = function () {
+		// Envío de To - Do
+		$("#tweet").submit(agregarToDo);
+	};
+
+	var agregarToDo = function (e) {
+		e.preventDefault();
+		// Obtenemos datos
+		var $contenedor = $("#posts");
+		var $mensajeContenedor = $("#message");
+		var $botonAgregar = $("#add-button");
+		var mensaje = $mensajeContenedor.val();
+
+		// Creamos elementos
+		var $postContenedor = $("<article />", { "class": "well" });
+		var $postCheck = $("<input type='checkbox' />");
+		var $postTexto = $("<p />");
+
+		var identificador = "marcador-" + contador;
+
+		// Personalizamos elementos
+		$postCheck.id = identificador;
+		$postTexto.attr("for", identificador);
+		$postTexto.text(mensaje);
+
+		// Agregarlos al DOM
+		$postContenedor.append($postCheck);
+		$postContenedor.append($postTexto);
+
+		// Agregarlo a un elemento existente para visualizarlo
+		$contenedor.prepend($postContenedor);
+
+
+		contador++;
+	};
+
+	// Cuando carga la página
+	$(document).ready(cargarPagina);
+})();
